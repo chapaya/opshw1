fb687711bdc7493d9fd69015aae783310e5a5f86
Eran G <egrimberg@gmail.com>
2017-11-01 14:46:39 +0200

    Update exercise3-fix.sh

--- vagrant/fixScripts/exercise3-fix.sh
+++ vagrant/fixScripts/exercise3-fix.sh
@@ -2,7 +2,10 @@
 #add fix to exercise3 here
 
 # need to install tomcat
-sudo apt-get install tomcat7
+sudo apt-get -y install apache2
 
 #restart tomcat service :
-sudo service tomcat7 restart
+sudo service http restart
+
+#test
+wget http://127.0.0.1
